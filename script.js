db09da4c014f1d16f1157df69e1730f64673dde1
michelle089 <68719954+michelle089@users.noreply.github.com>
2020-08-22 21:18:25 +0800

    Update script.js

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ for (var i = 0; i < names.length; i++) {
   var firstLetter = names[i].charAt(0).toLowerCase();
 
   if (firstLetter === 'j') {
-    GoodbyeSpeaker(names[i]);
+    byeSpeaker(names[i]);
   }
   else {
     helloSpeaker(names[i]);
