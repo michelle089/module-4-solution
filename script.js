219cd0c576d84e0f33a3798e4e79caf130f69598
michelle089 <68719954+michelle089@users.noreply.github.com>
2020-08-22 20:43:42 +0800

    Update script.js

--- script.js
+++ script.js
@@ -16,10 +16,10 @@ Good Bye Jim
     for (var i = 0; i < names.length; i++) {
         var firstLetter = names[i].charAt(0).toLowerCase();
         if (firstLetter === 'j') {
-            byeSpeaker.speak(names[i]);
+            goodbyeSpeaker.speak(names[i]);
         } 
         else {
             helloSpeaker.speak(names[i]);
         }
     }
-})()
+})();
